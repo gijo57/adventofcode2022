42e9190b7421c509a6e36171642aaba7dc29a8af
Pekka <pekka.tiitinen@kenjo.io>
2022-12-21 13:19:58 +0200

    add monkey copy for part 1

--- day21/advent.py
+++ day21/advent.py
@@ -1,7 +1,7 @@
 with open('input.txt') as f:
     lines = [line.strip() for line in f.readlines()]
 
-monkey_nums = {line[0:4]: line[6:] for line in lines}
+monkeys = {line[0:4]: line[6:] for line in lines}
 
 ops = {
     '+': lambda x, y: x + y,
@@ -12,21 +12,23 @@ ops = {
 
 
 def solve1():
-    names = list(monkey_nums.keys())
+    monkes = monkeys.copy()
+    names = list(monkes.keys())
     i = 0
 
     while True:
-        if (monkey_nums['root'].isnumeric()):
-            print(monkey_nums['root'])
+        if (monkes['root'].isnumeric()):
             break
 
         name = names[i]
-        num = monkey_nums[names[i]]
+        num = monkes[names[i]]
         if not num[-1].isnumeric():
             monkey1, op, monkey2 = num.split(' ')
-            if monkey_nums[monkey1].isnumeric() and monkey_nums[monkey2].isnumeric():
-                monkey_nums[name] = str(int(ops[op](int(monkey_nums[monkey1]), int(monkey_nums[monkey2]))))
+            if monkes[monkey1].isnumeric() and monkes[monkey2].isnumeric():
+                monkes[name] = str(int(ops[op](int(monkes[monkey1]), int(monkes[monkey2]))))
         i = (i + 1) % (len(names))
-
+    return monkes['root']
 
 answer1 = solve1()
+print(answer1)
+print(monkeys)
